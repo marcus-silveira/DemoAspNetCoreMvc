1e99dfc1f2690183b060ef8f1f4f020f5acabb9f
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-08-12 20:14:14 -0300

    First commit

---
+++ src/Demo/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers;
+
+public class HomeController : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        return View();
+    }
+}
