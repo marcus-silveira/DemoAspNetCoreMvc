c1090f7eaaa86b7f1c3eef121ed14344306ee544
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-08-16 21:06:24 -0300

    achado bug do pq de nao salvar os log do aspnet extension

--- src/Demo/Configuration/LoggingConfig.cs
+++ src/Demo/Configuration/LoggingConfig.cs
@@ -10,9 +10,9 @@ public static class LoggingConfig
         builder.Services.Configure<ElmahIoOptions>(builder.Configuration.GetSection("ElmahIo"));
         builder.Services.AddElmahIo();
         
-        builder.Logging.Services.Configure<ElmahIoProviderOptions>(builder.Configuration.GetSection("ElmahIo"));
+        builder.Services.Configure<ElmahIoProviderOptions>(builder.Configuration.GetSection("ElmahIo"));
         builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
-        builder.Logging.Services.AddElmahIo();
+        builder.Logging.AddElmahIo();
         
         builder.Logging.AddFilter<ElmahIoLoggerProvider>(null, LogLevel.Warning);
         
